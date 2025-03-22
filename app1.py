977844dcc0e2d2ef0c76df2a365d9888be792332
ryancode123 <ttthekhoicr7@gmail.com>
2025-03-22 19:28:56 +0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,18 +1,7 @@
-from flask import Flask, render_template, send_from_directory
-import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
-
-# Route để phục vụ file tĩnh
-@app.route('/static/<path:filename>')
-def serve_static(filename):
-    return send_from_directory('static', filename)
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
-
+    return "Hello, Render!"
